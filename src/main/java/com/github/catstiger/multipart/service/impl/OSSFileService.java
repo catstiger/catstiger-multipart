c16e40ef5adb40e100a2616daaf83d86cb03d34d
84e_100006150430_catstiger <catstiger@gmail.com>
2019-11-29 11:46:41 +0800

    OssFileService需要配置才可

--- src/main/java/com/github/catstiger/multipart/service/impl/OSSFileService.java
+++ src/main/java/com/github/catstiger/multipart/service/impl/OSSFileService.java
@@ -20,7 +20,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.util.FileCopyUtils;
 
 import com.aliyun.oss.OSSClient;
@@ -29,7 +28,7 @@ import com.aliyun.oss.model.ObjectMetadata;
 import com.github.catstiger.common.util.ContentTypes;
 import com.github.catstiger.common.util.UUIDHex;
 
-@Component
+//@Component
 public class OSSFileService extends AbstractFileService {
   private static Logger logger = LoggerFactory.getLogger(OSSFileService.class);
 
